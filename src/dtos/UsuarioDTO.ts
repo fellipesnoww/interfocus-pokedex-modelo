4ca9c56ec37672093301b31752a8539974327506
Fellipe Neves <fellipe2.0e@gmail.com>
2022-05-06 16:18:43 -0300

    Criado método para autenticar com IAS de testes

---
+++ src/dtos/UsuarioDTO.ts
@@ -0,0 +1,10 @@
+export interface UsuarioDTO {
+    access_token: string;
+    token: string;
+    expirationDate: Date;
+    refreshToken: string;
+    login:string;
+    usuarioId: number;
+    usuarioNome: string;
+    deviceToken: string;
+}
